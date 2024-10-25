aeb876af31dc984465f4c01201734fd334f31d56
imolena1984 <imolena@outlook.com.br>
2024-10-25 19:34:41 -0300

    Update script.js
    acerto

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+        function toggleMenu() {
+            var menu = document.getElementById("menu");
+            if (menu.style.left === "-250px") {
+                menu.style.left = "0";
+            } else {
+                menu.style.left = "-250px";
+            }
+        }
 
